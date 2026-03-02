a1eaa7767b51a88ee12c2f372ee4d04b31e5fd6d
Serge <sajghaa@gmail.com>
2026-03-02 19:57:49 +0200

    Updated views.py of blog app

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = (
+            Post.objects
+             .filter(status='published')
+             .select_related('author', 'category')
+        )
+
+        category_slug = self.request.GET.get('category')
+
+        if category_slug:
+            queryset =  queryset.filter(category_slug=category_slug)
+
+        return queryset
