21598f9db6093e4ec46d3d878d1bf0b1437dcf45
Serge <sajghaa@gmail.com>
2026-03-03 20:57:46 +0200

    Updated urls.py in blog_app

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostCreateView,
+    PostDetailView,
+    PostUpdateView,
+    PostDeleteView,
+    )
 
 app_name ="apps.blog"
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('<slug:slug>/update/', PostUpdateView.as_view(), name='post_update'),
+    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post_detail'),
 ]
