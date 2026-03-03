db6ac865b45f97fe61239c4d693d030cc7d12c03
Serge <sajghaa@gmail.com>
2026-03-03 21:42:06 +0200

    updated config/urls.py & settings/base.py and some templates

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication settings
+LOGIN_REDIRECT_URL = 'blog:post_list'
+LOGOUT_REDIRECT_URL = 'core:home'
+LOGIN_URL = 'login'
--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('blog/', include('apps.blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
