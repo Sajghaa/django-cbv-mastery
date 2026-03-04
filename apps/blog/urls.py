c8fcf654784fb077c0ae9baef44df7d910eee2b8
Serge <sajghaa@gmail.com>
2026-03-04 21:47:28 +0200

    Updated blog/urls.py

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('<slug:slug>/update/', PostUpdateView.as_view(), name='post_update'),
-    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post_detail'),
+    path('<slug:slug>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
